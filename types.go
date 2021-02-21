917c45a5f37659278075ae706053c0790ffd904e
bilalcaliskan <bilalcaliskan@protonmail.com>
2021-02-21 22:30:23 +0300

    fixes #6: pflag used

--- types.go
+++ types.go
@@ -59,28 +59,3 @@ func (worker *Worker) Equals(other *Worker) bool {
 	isHostIPEquals := worker.HostIP == other.HostIP
 	return isMasterIPEquals && isHostIPEquals
 }
-
-/*func (cluster *Cluster) Equals(other *Cluster) bool {
-	isMasterIPEquals := cluster.MasterIP == other.MasterIP
-	isWorkersEqual := len(cluster.Workers) == len(other.Workers)
-	if isWorkersEqual {  // copy slices so sorting won't affect original structs
-		backendWorkers := make([]Worker, len(backend.Workers))
-		otherWorkers := make([]Worker, len(other.Workers))
-		copy(backend.Workers, backendWorkers)
-		copy(other.Workers, otherWorkers)
-		// Sort by index, keeping original order or equal elements.
-		sort.SliceStable(backendWorkers, func(i, j int) bool {
-			return backendWorkers[i].Index < backendWorkers[j].Index
-		})
-		sort.SliceStable(otherWorkers, func(i, j int) bool {
-			return otherWorkers[i].Index < otherWorkers[j].Index
-		})
-		for index, item := range backendWorkers {
-			if item != otherWorkers[index] {
-				isWorkersEqual = false
-			}
-		}
-	}
-	// return isNameEquals && isIpEquals && isPortEquals && isWorkersEqual
-	return isNameEquals && isIpEquals && isPortEquals
-}*/
